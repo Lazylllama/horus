690f5113b456574268d9777084ae0c9eaeeaf9ed
Simon Klövdahl <me@lazyllama.xyz>
2026-07-06 03:08:57 +0200

    fix: that didnt work in preview...

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -6,6 +6,8 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}` || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_BASE_URL
+    || `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+    || "http://localhost:3000",
   plugins: [genericOAuthClient(), inferAdditionalFields<typeof auth>()],
 });
