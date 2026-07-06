1ebf5633384cebd7b280efc35a7956657ca144c9
Simon Klövdahl <me@lazyllama.xyz>
2026-07-06 03:12:32 +0200

    fix: ah

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -6,7 +6,7 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL
     || `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
     || "http://localhost:3000",
   plugins: [genericOAuthClient(), inferAdditionalFields<typeof auth>()],
