869ad35e144efafd0859b7ec702150d77542e81b
Lazyllama <me@lazyllama.xyz>
2026-07-22 06:24:23 +0200

    fix: cron secret header

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -9,16 +9,7 @@ import { getStats } from "@/lib/nephthys";
 export async function POST() {
   const headersData = await headers();
 
-  console.log(
-    headersData.forEach((value, key) => {
-      console.log(`${key}: ${value}`);
-    }),
-  );
-  if (
-    headersData.get("Upstash-Forward-Cron-Secret") !==
-      `Bearer ${process.env.CRON_SECRET}` &&
-    headersData.get("Cron-Secret") !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
+  if (headersData.get("cron-secret") !== `${process.env.CRON_SECRET}`) {
     return new Response("Unauthorized", { status: 401 });
   }
 
