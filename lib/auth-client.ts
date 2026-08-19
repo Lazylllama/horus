5ac2d1f66c1fb9b5d139ad032ff8fa455f4b187e
Simon K <me@lazyllama.xyz>
2026-08-19 20:10:16 +0200

    fix: no more vercel, no more vercel vars

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -9,11 +9,7 @@ import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL:
-    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
-    (process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL &&
-      `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`) ||
-    "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [
     genericOAuthClient(),
     organizationClient(),
