43dda44ef69d27497b9ac835984af8163e6ddc78
Simon K <me@lazyllama.xyz>
2026-08-19 21:44:52 +0200

    feat: find real ip

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,3 +1,14 @@
 import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "@/lib/auth"; // path to your auth file
 export const { POST, GET } = toNextJsHandler(auth);
+
+export async function middleware(req: Request) {
+  console.log("Request headers:", {
+    "x-forwarded-for": req.headers.get("x-forwarded-for"),
+    "x-real-ip": req.headers.get("x-real-ip"),
+    "cf-connecting-ip": req.headers.get("cf-connecting-ip"),
+    "user-agent": req.headers.get("user-agent"),
+    Rest: req.headers,
+  });
+  return undefined;
+}
