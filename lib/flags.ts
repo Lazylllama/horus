87fe67ea5084cdc08ecdeaf5c8957f2f1a601e40
Simon K <me@lazyllama.xyz>
2026-08-19 20:23:59 +0200

    fix: less posthog env variables

--- lib/flags.ts
+++ lib/flags.ts
@@ -1,5 +1,12 @@
-import { postHogAdapter } from "@flags-sdk/posthog";
+import { createPostHogAdapter } from "@flags-sdk/posthog";
 import { flag } from "flags/next";
+
+const postHogAdapter = createPostHogAdapter({
+  postHogKey: process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN || "",
+  postHogOptions: {
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "",
+  },
+});
 
 export const marmaladeFlag = flag({
   key: "marmalade",
