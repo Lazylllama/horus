9fd3de5de5d78381499d8a23ab0dc03c3596f144
Simon K <me@lazyllama.xyz>
2026-08-19 20:44:11 +0200

    feat: testing in prod :fire:

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -8,6 +8,7 @@ import {
 import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
+console.log("USING BASEURL:", process.env.NEXT_PUBLIC_BETTER_AUTH_URL);
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [
