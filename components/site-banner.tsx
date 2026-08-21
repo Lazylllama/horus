d17b6bd1c8bcc693fa04514086d48ed66891b8f7
Simon K <me@lazyllama.xyz>
2026-08-21 16:51:18 +0200

    feat: update banner

--- components/site-banner.tsx
+++ components/site-banner.tsx
@@ -2,11 +2,11 @@ import { MessageSquareWarningIcon } from "lucide-react";
 
 export function SiteBanner() {
   return (
-    <div className="bg-destructive text-primary-foreground">
+    <div className="bg-primary text-primary-foreground">
       <div className="items-center justify-center px-4 py-2 flex flex-row gap-2">
         <MessageSquareWarningIcon size={18} />
         <p className="text-center text-sm">
-          Open-beta software, bugs commonly appear.
+          Is your instance missing? reach out and have it added!
         </p>
       </div>
     </div>
